0529711598f5287f4136793f6b1e2943108737d1
kerlosbahaa3 <aboelking4@gmail.com>
2023-04-24 15:58:47 +0200

    add js parts

---
+++ java.js
@@ -0,0 +1,23 @@
+let arr = [
+  (object = {
+    title: "adventure time",
+    author: "ali baba ",
+  }),
+  (object = {
+    title: "adventure time2",
+    author: "ali baba2 ",
+  }),
+];
+let add = getElementById("add");
+let div = getElementById("books");
+let title = getElementById("title");
+let author = getElementById("author");
+function adding(title,author ){
+
+}
+
+add.onclick()=function(){
+  div.innerHTML = ` <h3>Code and Game</h3>
+  <p>Safari Hamuli</p>
+  <button>Remove</button>`;
+}
