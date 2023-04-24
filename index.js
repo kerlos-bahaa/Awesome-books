75bcaed2f9d187697e9558bca00667b0fff22b00
kerlosbahaa3 <aboelking4@gmail.com>
2023-04-24 17:29:22 +0200

    remove button

--- index.js
+++ index.js
@@ -6,29 +6,32 @@ let div = document.getElementById("books");
 let title = document.getElementById("title");
 let author = document.getElementById("author");
 
-function adding(title , author ){
-   books.push({title: title.value ,  author : author.value });
-   localStorage.setItem("Books" , JSON.stringify(books));
-   let localBooks = JSON.parse(localStorage.getItem('Books'));
-   div.innerHTML=""
+function adding(title, author) {
+  books.push({ title: title.value, author: author.value });
+  localStorage.setItem("Books", JSON.stringify(books));
+  let localBooks = JSON.parse(localStorage.getItem("Books"));
+  div.innerHTML = "";
 
-   for(let i = 0 ; i < localBooks.length ; i++){
+  for (let i = 0; i < localBooks.length; i++) {
     div.innerHTML += `
         <h3>${localBooks[i].title}</h3>
         <p>${localBooks[i].author}</p>
-        <button>Remove</button>
+        <button id="Remove">Remove</button>
         <br>
         <br>
-      ` 
-   };
-   console.log(localBooks)
-};
+      `;
+  }
+  console.log(localBooks);
+}
 
-document.getElementById('add').addEventListener('click' , ()=>{
-  adding(title , author);
+document.getElementById("add").addEventListener("click", () => {
+  adding(title, author);
 });
-
-
+// let Remove = document.getElementById("Remove");
+document.getElementById("Remove").addEventListener("click", () => {
+  let RemoveFunction = JSON.parse(localStorage.getElementById("Books"));
+  console.log(RemoveFunction);
+});
 // add.onclick()=function(){
 //   div.innerHTML = ` <h3>Code and Game</h3>
 //   <p>Safari Hamuli</p>
