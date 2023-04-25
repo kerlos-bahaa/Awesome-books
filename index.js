0903a0d06715ec7038bf6edbd7af2a60d3252021
kerlosbahaa3 <aboelking4@gmail.com>
2023-04-25 09:47:27 +0200

    activate funtion

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-class awesomeBooks{
-      books;
-      booksContainer ;
-      title;
-      author;
+class awesomeBooks {
+  books;
+  booksContainer;
+  title;
+  author;
 
-      constructor(){
-        this.books = [];
-        this.booksContainer = document.getElementById('books');
-        this.title  = document.getElementById('title');
-        this.author  = document.getElementById('author');
-      }
+  constructor() {
+    this.books = [];
+    this.booksContainer = document.getElementById("books");
+    this.title = document.getElementById("title");
+    this.author = document.getElementById("author");
+  }
 
-       localBooks(books) {
-        const storage = JSON.parse(localStorage.getItem('Books'));
-        return storage || books;
-      }
+  localBooks(books) {
+    const storage = JSON.parse(localStorage.getItem("Books"));
+    return storage || books;
+  }
 
-       displayBooks(booksList) {
-        this.booksContainer.innerHTML = '';
-        for (let i = 0; i < booksList.length; i += 1) {
-          this.booksContainer.innerHTML += `
+  displayBooks(booksList) {
+    this.booksContainer.innerHTML = "";
+    for (let i = 0; i < booksList.length; i += 1) {
+      this.booksContainer.innerHTML += `
             <br>
             <p>${booksList[i].title}</p>
             <p>${booksList[i].author}</p>
@@ -27,30 +27,39 @@ class awesomeBooks{
             <hr>
             <br/>
           `;
-        }
-      }
+    }
+  }
 
-       adding() {
-        if (this.title.value.length > 1 && this.author.value.length > 1) {
-          this.books = this.localBooks(this.books);
-          this.books.push({ id: Date.now(), title: this.title.value, author: this.author.value });
-          localStorage.setItem('Books', JSON.stringify(this.books));
-          this.displayBooks(this.localBooks(this.books));
-          this.title.value = '';
-          this.author.value = '';
-        }
-      }
+  adding() {
+    if (this.title.value.length > 1 && this.author.value.length > 1) {
+      this.books = this.localBooks(this.books);
+      this.books.push({
+        id: Date.now(),
+        title: this.title.value,
+        author: this.author.value,
+      });
+      localStorage.setItem("Books", JSON.stringify(this.books));
+      this.displayBooks(this.localBooks(this.books));
+      this.title.value = "";
+      this.author.value = "";
+    }
+  }
 
-       removeItem(items) {
-        if (items !== undefined) {
-          const localItem = this.localBooks(this.books);
-          const removedItem = localItem.filter((item) => item.id !== localItem[items].id);
-          localStorage.setItem('Books', JSON.stringify(removedItem));
-          return this.displayBooks(this.localBooks(books));
-        }
-        return this.displayBooks(this.localBooks(books));
-      }
+  removeItem(items) {
+    if (items !== undefined) {
+      const localItem = this.localBooks(this.books);
+      const removedItem = localItem.filter(
+        (item) => item.id !== localItem[items].id
+      );
+      localStorage.setItem("Books", JSON.stringify(removedItem));
+      return this.displayBooks(this.localBooks(books));
+    }
+    return this.displayBooks(this.localBooks(books));
+  }
 }
 
-
-
+const awesome_books = new awesomeBooks();
+awesome_books.removeItem();
+document.getElementById("add").addEventListener("click", () => {
+  awesome_books.adding();
+});
